db5af8b41ec909f1c1e0404366d1562a736ca613
herreramaxi <herreramaxi@gmail.com>
2024-03-19 23:39:19 +0000

    chatgpt example using httpclient

---
+++ ChatGptTests/ChatGPTApiExample.cs
@@ -0,0 +1,84 @@
+﻿using Ardalis.Result;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace ChatGptTests
+{
+    public interface IChatGPTApiExample {
+        Task<Result<string>> ExplainCodeAsync(string codeSnippet);
+    }
+    public class ChatGPTApiExample: IChatGPTApiExample
+    {
+        private readonly ILogger<ChatGPTApiExample> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ChatGPTApiExample(ILogger<ChatGPTApiExample> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task<Result<string>> ExplainCodeAsync(string codeSnippet)
+        {
+            var prompt = "Please explain the following code:";
+            var apikey = _configuration["API_KEY"];
+            var apiUrl = "https://api.openai.com/v1/chat/completions";
+            var model = "gpt-4-turbo-preview";
+            var temperature = 0.3;
+
+            var messages = new[] {
+                new
+                {
+                    role= "user",
+                    content=$"{prompt}\n\n{codeSnippet}"
+                }
+            };
+
+            var data = new
+            {
+                model = model,
+                messages = messages,
+                temperature = temperature
+            };
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apikey.Trim());
+            //httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer: {apikey}");
+            var json = JsonSerializer.Serialize(data);
+            var requestContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await httpClient.PostAsync(apiUrl, requestContent);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogError("ChatGPT API response did not contain expected content");
+                    return Result.Error("ChatGPT description could not be loaded");
+                }
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(responseContent);
+                var choicesElement = doc.RootElement.GetProperty("choices");
+                var messageObject = choicesElement[0].GetProperty("message");
+                var content = messageObject.GetProperty("content").GetString();
+
+                if (content is null)
+                {
+                    _logger.LogError("ChatGPT API response did not contain expected content");
+                    return Result.Error("ChatGPT description could not be loaded");
+                }
+
+                return Result.Success(content);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"ChatGPT error occurred: {ex.Message}");
+                return Result.Error("ChatGPT description could not be loaded");
+            }
+        }
+    }
+}
---
+++ ChatGptTests/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ChatGptTests
+{
+    internal class Program
+    {
+        public const string CODE = @"from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model=""gpt-3.5-turbo"",
+  messages=[
+    {""role"": ""system"", ""content"": ""You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.""},
+    {""role"": ""user"", ""content"": ""Compose a poem that explains the concept of recursion in programming.""}
+  ]
+)
+
+print(completion.choices[0].message)";
+        static async Task Main(string[] args)
+        {
+            // Set up configuration
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Set up Serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console()
+                 //.WriteTo.File(logFileName, rollingInterval: RollingInterval.Day)
+                 .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting application");
+
+                // Create service provider
+                var serviceProvider = new ServiceCollection()
+                    .AddSingleton<IConfiguration>(configuration)
+                    .AddSingleton<IChatGPTApiExample, ChatGPTApiExample>()
+                    .AddLogging(builder =>
+                    {
+                        builder.AddSerilog();
+                    })
+                    .BuildServiceProvider();
+
+                // Resolve service
+                var myService = serviceProvider.GetService<IChatGPTApiExample>();
+                var response = await myService.ExplainCodeAsync(CODE);
+                Log.Information($"Response: {response}");
+
+                Log.Information("Application ended successfully");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
